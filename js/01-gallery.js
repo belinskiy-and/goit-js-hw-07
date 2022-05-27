07032f7492a391c2e863d06b8ca525c4eeec1ba8
belinskiy-and <90721066+belinskiy-and@users.noreply.github.com>
2022-05-27 21:50:32 +0300

    Work homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryElement = document.querySelector(".gallery");
+
+for (const galleryItem of galleryItems) {
+    // <div>
+    const newItem = document.createElement("div");
+
+    newItem.classList.add("gallery__item");
+
+    galleryElement.append(newItem);
+
+    // <a>
+    const linkElement = document.createElement("a");
+
+    linkElement.classList.add("gallery__link");
+    linkElement.setAttribute("href", galleryItem.original);
+
+    newItem.append(linkElement);
+
+    // <img>
+    const image = document.createElement("img");
+    
+    image.classList.add("gallery__image");
+    image.setAttribute("src", galleryItem.preview);
+    image.setAttribute("data-source", galleryItem.original);
+    image.setAttribute("alt", galleryItem.description);
+
+    image.style.cursor = "zoom-in";
+
+    linkElement.append(image);
+}
+
+const clickAction = event => {
+    event.preventDefault();
+    const original = event.target.getAttribute("data-source");
+
+    console.log(original);
+
+    basicLightbox.create(`
+		<img width="1400" height="900" src="${original}">
+	`).show()
+};
+
+galleryElement.addEventListener("click", clickAction);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryElement = document.querySelector(".gallery");
+
+for (const galleryItem of galleryItems) {
+    // <div>
+    // const newItem = document.createElement("div");
+
+    // newItem.classList.add("gallery__item");
+
+    // galleryElement.append(newItem);
+
+    // <a>
+    const linkElement = document.createElement("a");
+
+    linkElement.classList.add("gallery__item");
+    linkElement.setAttribute("href", galleryItem.original);
+
+    galleryElement.append(linkElement);
+
+    // <img>
+    const image = document.createElement("img");
+    
+    image.classList.add("gallery__image");
+    image.setAttribute("src", galleryItem.preview);
+    image.setAttribute("alt", galleryItem.description);
+
+    image.style.cursor = "zoom-in";
+
+    linkElement.append(image);
+}
+
+const clickAction = event => {
+    event.preventDefault();
+    const original = event.target.getAttribute("data-source");
+
+    console.log(original);
+
+    let gallery = new SimpleLightbox('.gallery a', {
+        // captionType: "text",
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+    gallery.on('show.simplelightbox', function () {
+	// do something…
+});
+};
+
+galleryElement.addEventListener("click", clickAction);
